0dafbb0b3f588e7d3b9ad2c31c511ecb6a5d64fb
jaqee-a <bouderbalamohammed2000@gmail.com>
2024-09-13 00:44:52 +0100

    block mod line movement

--- init.lua
+++ init.lua
@@ -12,3 +12,6 @@ vim.keymap.set("n", "<A-k>", "<Esc>:m .-2<CR>==", {});
 
 vim.keymap.set("i", "<A-j>", "<Esc>:m .+1<CR>==gi", {});
 vim.keymap.set("i", "<A-k>", "<Esc>:m .-2<CR>==gi", {});
+
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv-gv", {});
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv-gv", {});
